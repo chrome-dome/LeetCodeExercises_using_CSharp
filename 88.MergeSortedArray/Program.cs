3c71a94e67711cdf87f6fd1b71f2c3ac5751ceed
chrome-dome <chrome-domepl@protonmail.com>
2023-01-24 15:05:38 +0100

    add task 88

---
+++ 88.MergeSortedArray/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+- You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+- Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ */
+
+
+void merge(int[] nums1, int m, int[] nums2, int n)
+{
+    int i = m - 1, j = n - 1, k = m + n - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] > nums2[j])
+        {
+            nums1[k--] = nums1[i--];
+        }
+        else
+        {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    while (j >= 0)
+    {
+        nums1[k--] = nums2[j--];
+    }
+}
+
+int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+int[] nums2 = new int[] { 2, 5, 6 };
+int m = 3, n = 3;
+merge(nums1, m, nums2, n);
+
+foreach (int num in nums1)
+{
+    Console.Write(num + " ");
+}
+Console.WriteLine();
+Console.ReadLine();
