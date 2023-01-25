dafaba9ec0ed7d119bb9f4649d945a23e192fe31
chrome-dome <chrome-domepl@protonmail.com>
2023-01-25 16:24:40 +0100

    add task 169

---
+++ 169.MajorityElement/Program.cs
@@ -0,0 +1,31 @@
+﻿//Given an array nums of size n, return the majority element.
+
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+int FindMajorityElement(int[] nums)
+{
+    int majorityElement = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] == majorityElement)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+            if (count == 0)
+            {
+                majorityElement = nums[i];
+                count = 1;
+            }
+        }
+    }
+    return majorityElement;
+}
+
+int[] nums = { 3, 2, 3 };
+int majorityElement = FindMajorityElement(nums);
+Console.WriteLine("The majority element is: " + majorityElement);
+Console.ReadLine();
