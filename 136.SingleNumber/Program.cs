d7496da5c6a537033443607934629e3767bcc12d
chrome-dome <chrome-domepl@protonmail.com>
2023-01-25 16:21:52 +0100

    add task 136

---
+++ 136.SingleNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+-Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+-You must implement a solution with a linear runtime complexity and use only constant extra space.
+ */
+
+int SingleNumber(int[] nums)
+{
+    int result = 0;
+    foreach (int num in nums)
+    {
+        //the property of the XOR operator that a ^a = 0 and a ^0 = a
+        result ^= num;
+    }
+    return result;
+}
+
+int[] nums = { 2, 2, 1 };
+int single = SingleNumber(nums);
+Console.WriteLine(single); // Output: 1
+Console.ReadLine();
