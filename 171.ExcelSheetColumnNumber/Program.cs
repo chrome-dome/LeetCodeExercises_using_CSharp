a3a72d4fba9f4ee43c116f92b8f8ea0738550252
chrome-dome <chrome-domepl@protonmail.com>
2023-01-26 20:20:09 +0100

    add task 171

---
+++ 171.ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿//Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+using System;
+
+class ExcelColumns
+{
+    public static int ColumnTitleToNumber(string columnTitle)
+    {
+        int columnNumber = 0;
+
+        for (int i = 0; i < columnTitle.Length; i++)
+        {
+            columnNumber = columnNumber * 26 + (columnTitle[i] - 'A' + 1);
+        }
+
+        return columnNumber;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(ColumnTitleToNumber("A")); // prints 1
+        Console.WriteLine(ColumnTitleToNumber("B")); // prints 2
+        Console.WriteLine(ColumnTitleToNumber("Z")); // prints 26
+        Console.WriteLine(ColumnTitleToNumber("AA")); // prints 27
+        Console.WriteLine(ColumnTitleToNumber("AB")); // prints 28
+        Console.ReadLine();
+    }
+}
