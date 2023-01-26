9db658c92163acd86f9d51001ac5c89ed76faef3
chrome-dome <chrome-domepl@protonmail.com>
2023-01-26 20:23:45 +0100

    add task 191

---
+++ 191.NumberOf1Bits/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+using System;
+
+class Program
+{
+    static int HammingWeight(uint n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            count += (int)(n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+
+    static void Main()
+    {
+        uint n = 00000000000000000000000000001011;
+        Console.WriteLine("Hamming weight of " + n + " is: " + HammingWeight(n));
+        Console.ReadLine();
+    }
+}
