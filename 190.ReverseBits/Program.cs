e98469dfcb4aaa345574f4ea17a34e46f243e382
chrome-dome <chrome-domepl@protonmail.com>
2023-01-26 20:22:26 +0100

    add task 190

---
+++ 190.ReverseBits/Program.cs
@@ -0,0 +1,27 @@
+﻿//Reverse bits of a given 32 bits unsigned integer.
+
+using System;
+
+class ReverseBits
+{
+    static void Main(string[] args)
+    {
+        uint n = 43261596; //00000010100101000001111010011100
+        uint reversed = ReverseBitsHelper(n);
+        Console.WriteLine("Original: {0}", n);
+        Console.WriteLine("Reversed: {0}", reversed);
+        Console.ReadLine();
+    }
+
+    static uint ReverseBitsHelper(uint n)
+    {
+        uint reversed = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            reversed = (reversed << 1) | (n & 1);
+            n >>= 1;
+        }
+        return reversed;
+    }
+}
+
