b10e34b3cbe645039e264d66bf4764d4b998db05
chrome-dome <chrome-domepl@protonmail.com>
2023-01-27 16:26:57 +0100

    add task 234

---
+++ 234.PalindromeLinkedList/Program.cs
@@ -0,0 +1,46 @@
+﻿//Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+using System;
+using System.Collections.Generic;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class Solution
+{
+
+    public static void Main()
+    {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        Solution solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(head));
+        Console.ReadLine();
+    }
+
+    public bool IsPalindrome(ListNode head)
+    {
+        if (head == null) return true;
+        var list = new List<int>();
+        while (head != null)
+        {
+            list.Add(head.val);
+            head = head.next;
+        }
+        for (int i = 0; i < list.Count / 2; i++)
+        {
+            if (list[i] != list[list.Count - i - 1]) return false;
+        }
+        return true;
+    }
+}
+
+
+
