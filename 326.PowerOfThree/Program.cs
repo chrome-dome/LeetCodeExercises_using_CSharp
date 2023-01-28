f7daf867aed3b7ceced4ec57e673df13202897a7
chrome-dome <chrome-domepl@protonmail.com>
2023-01-28 22:34:52 +0100

    add task 326

---
+++ 326.PowerOfThree/Program.cs
@@ -0,0 +1,24 @@
+﻿//Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+//An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+
+bool IsPowerOfThree(int n)
+{
+    if (n < 1)
+    {
+        return false;
+    }
+    while (n % 3 == 0)
+    {
+        n /= 3;
+    }
+    return n == 1;
+}
+
+
+Console.WriteLine(IsPowerOfThree(27)); // Output: true
+Console.WriteLine(IsPowerOfThree(81)); // Output: true
+Console.WriteLine(IsPowerOfThree(4)); // Output: false
+
+Console.ReadLine();
